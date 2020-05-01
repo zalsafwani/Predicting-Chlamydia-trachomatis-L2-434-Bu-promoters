2dde2c92ab59569ffd6830a052d55239c2cf57a2
zalsafwani <61522855+zalsafwani@users.noreply.github.com>
2020-05-01 02:53:25 -0500

    Add files via upload

---
+++ protein.py
@@ -0,0 +1,35 @@
+import MySQLdb  
+from Bio import Entrez, SeqIO, GenBank
+import re
+
+Entrez.email = "zalsafwani@unomaha.edu"
+conn = MySQLdb.connect(db="zalsafwani")
+cursor = conn.cursor()
+
+cursor.execute("select locus_tag, gene_symbol from coding_sequence where gene_symbol != '-';")
+row = cursor.fetchall()
+outFile = open("protein_info", "w")
+for data in row:
+    locus = data[0] + "\t"
+    symbol = data[1] + "\t"
+    
+    handle = Entrez.esearch(db="protein" ,term=data[0])
+    record = Entrez.read(handle)
+    if len(record['IdList']) == 2:
+        idnum = (record['IdList'][1])
+    else:
+        idnum = (record['IdList'][0])
+    handle.close()
+    
+    handle2 = Entrez.efetch(db="protein", id=idnum, rettype="gb", retmode="text")
+    for record in GenBank.parse(handle2):
+        version = record.version + "\t" 
+        length = record.size + "\t" 
+        fullinfo = (record._definition_line()).strip()
+     
+        fullinfo = fullinfo.replace("DEFINITION ", "").strip()
+        fullinfo = re.sub("\[Chlamydia.*\n*.*].", "", fullinfo).strip()
+        insert =  locus + version  + symbol + length + fullinfo + '\n'
+        outFile.write(insert)
+    handle2.close()
+outFile.close()
