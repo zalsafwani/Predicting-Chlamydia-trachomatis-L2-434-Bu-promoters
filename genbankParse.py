19d7e0dc01fc6eecfbb681a144df499b54ab27ba
zalsafwani <61522855+zalsafwani@users.noreply.github.com>
2020-05-01 02:54:36 -0500

    Update genbankParse.py

--- genbankParse.py
+++ genbankParse.py
@@ -1,5 +1,14 @@
 # Import necessary libraries
 from Bio import GenBank
+from Bio import Entrez
+from Bio import SeqIO
+
+Entrez.email = "zalsafwani@unomaha.edu"
+search = Entrez.efetch(db="nucleotide", id="AM884176.1", rettype="gb", retmode="text")
+searchin = open("chamydia_trachomatisL2_434_Bu.gb","w")
+searchin.write(search.read())
+searchin.close()
+
 
 # Open the genbank file to parse through it
 # then open a file in write mood to save the data neaded 
@@ -15,10 +24,17 @@ with open("chamydia_trachomatisL2_434_Bu.gb") as handle:
         
         # Go to the features section to get all the genes
         # and store the information needed for coding_sequence table
+        fileOut2 = open("genes.list","w")
         for current in record.features:
             if current.key == "gene":
-                print(current)
-
+                fileOut2.write(str(current))
+        fileOut2.close()
 handle.close()
 
-
+clSeq = ""
+fileOut3 = open("sequence.fasta","w")
+fileOut3.write(">AM884176.1 Chlamydia trachomatis strain L2/434/Bu complete genome\n")
+for seq_record in SeqIO.parse("chamydia_trachomatisL2_434_Bu.gb", "genbank"):
+    clSeq = str(seq_record.seq)
+    fileOut3.write(clSeq)
+fileOut3.close()
