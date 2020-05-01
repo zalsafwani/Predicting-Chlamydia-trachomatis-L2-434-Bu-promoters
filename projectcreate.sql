ee28f76cf496e257c25b63ea1335dca362feb116
zalsafwani <61522855+zalsafwani@users.noreply.github.com>
2020-05-01 02:55:38 -0500

    Update projectcreate.sql

--- projectcreate.sql
+++ projectcreate.sql
@@ -1,14 +1,13 @@
-
 /* Choose the database name USE (name);
  * If these tables exist already, delete them.
  * Careful with this type of statement, 
  * especially if you have been collecting data 
  * regularly and don't have it backed up.
  */
---USE zalsafwani;
+USE zalsafwani;
 DROP TABLE IF EXISTS chlamydia_trachomatis,
 		     promoters_prediction,
-		     protein_aliases,
+		     protein_info,
 		     coding_sequence;
 
 /* DDL statements are below for the 
@@ -18,7 +17,7 @@ DROP TABLE IF EXISTS chlamydia_trachomatis,
  */
 CREATE TABLE chlamydia_trachomatis(
     accession_number    VARCHAR(20)     NOT NULL,
-    basepairs           VARCHAR(25)     NOT NULL,
+    length_bp           VARCHAR(25)     NOT NULL,
     dna_type            VARCHAR(20)     NOT NULL,
     definition          VARCHAR(100)     NOT NULL,
     orgnism             VARCHAR(100)     NOT NULL,
@@ -27,7 +26,7 @@ CREATE TABLE chlamydia_trachomatis(
 
 CREATE TABLE coding_sequence(
     locus_tag           VARCHAR(20)     NOT NULL,
-    gene_name           VARCHAR(20),
+    gene_symbol         VARCHAR(20),
     strand              VARCHAR(20)     NOT NULL,
     start_position      INT             NOT NULL,
     end_position        INT             NOT NULL,
@@ -37,19 +36,20 @@ CREATE TABLE coding_sequence(
 
 CREATE TABLE promoters_prediction(
     locus_tag           VARCHAR(20)     NOT NULL,
-    promoters_ID        VARCHAR(20)     NOT NULL,
     strand              VARCHAR(20)     NOT NULL,
     start_position      INT             NOT NULL,
     end_position        INT             NOT NULL,
     sequence            VARCHAR(25)     NOT NULL,
     FOREIGN KEY (locus_tag) REFERENCES coding_sequence (locus_tag) ON DELETE CASCADE, 
-    PRIMARY KEY (promoters_ID)
+    PRIMARY KEY (locus_tag)
     );
 
-CREATE TABLE protein_aliases(
+CREATE TABLE protein_info(
     locus_tag		VARCHAR(20)	NOT NULL,
-    gene_name		VARCHAR(20)	NOT NULL,
-    aliases		VARCHAR(50)	NOT NULL,
-    FOREIGN KEY (locus_tag) REFERENCES coding_sequence (locus_tag) ON DELETE CASCADE
+    accession_number	VARCHAR(20)	NOT NULL,
+    gene_symbol		VARCHAR(20),
+    length_aa           INT             NOT NULL,
+    full_name		VARCHAR(50)	NOT NULL,
+    FOREIGN KEY (locus_tag) REFERENCES coding_sequence (locus_tag),
+    PRIMARY KEY (accession_number)
     );
- 
