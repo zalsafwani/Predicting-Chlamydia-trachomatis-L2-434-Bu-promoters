8622d121f9c353039577410687ee4fa8ef2dd4b2
zalsafwani <61522855+zalsafwani@users.noreply.github.com>
2020-05-04 03:36:09 -0500

    Update runcommand.sh

--- runcommand.sh
+++ runcommand.sh
@@ -6,5 +6,5 @@ python3 parsecodingseq.py
 mysqlimport zalsafwani coding_sequence -L
 python3 phiSITEFilterResults.py
 mysqlimport zalsafwani promoters_prediction -L
-python3 protein.py
-mysqlimport zalsafwani protein_info -L
+python3 geneInfo.py
+mysqlimport zalsafwani gene_info -L
