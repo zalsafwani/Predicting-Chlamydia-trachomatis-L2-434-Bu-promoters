48061e465a4b10b58b83e1a08858a16a391f2a73
zalsafwani <61522855+zalsafwani@users.noreply.github.com>
2020-05-06 01:49:38 -0500

    Add comments

--- analysis.sql
+++ analysis.sql
@@ -1,5 +1,8 @@
+-- Choose the database name
 USE zalsafwani;
 
+-- Select everything from gene_info table
 SELECT * FROM gene_info;
 
+-- Select everything from promoters_prediction table
 SELECT * FROM promoters_prediction;
